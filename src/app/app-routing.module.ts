2bd3e43d1d2fcf230d35acbefa3f101772e43efd
BannyT <tumusiimebarnabus@gmail.com>
2021-07-13 15:24:57 +0300

    data update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { ShopsComponent } from './pages/shops/shops.component';
 
 const routes: Routes = [
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: '',redirectTo: 'home',pathMatch: 'full'},
   { path: 'login', component:LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'shops', component:ShopsComponent },
   
   
 ];
