5106c6d5079aadc3fa5ab1181d083239ffe0d5c7
BannyT <tumusiimebarnabus@gmail.com>
2021-07-15 15:06:52 +0300

    adedded update

--- src/app/pages/shop/shop-routing.module.ts
+++ src/app/pages/shop/shop-routing.module.ts
@@ -7,6 +7,22 @@ const routes: Routes = [
   {
     path: '',
     component: ShopPage
+  },
+  {
+    path: 'orders',
+    loadChildren: () => import('./orders/orders.module').then( m => m.OrdersPageModule)
+  },
+  {
+    path: 'products',
+    loadChildren: () => import('./products/products.module').then( m => m.ProductsPageModule)
+  },
+  {
+    path: 'gallery',
+    loadChildren: () => import('./gallery/gallery.module').then( m => m.GalleryPageModule)
+  },
+  {
+    path: 'aboutshop',
+    loadChildren: () => import('./aboutshop/aboutshop.module').then( m => m.AboutshopPageModule)
   }
 ];
 
--- src/app/pages/shop/shop.module.ts
+++ src/app/pages/shop/shop.module.ts
@@ -4,17 +4,81 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
-import { ShopPageRoutingModule } from './shop-routing.module';
+import { Routes, RouterModule } from '@angular/router';
 
 import { ShopPage } from './shop.page';
+import { GalleryPage } from './gallery/gallery.page';
+import { ProductsPage } from './products/products.page';
+import { AboutshopPage } from './aboutshop/aboutshop.page';
+import { OrdersPage } from './orders/orders.page';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ShopPage,
+    children: [
+      {
+        path: 'gallery',
+        children: [
+          {
+            path: '',
+            component:GalleryPage
+          }
+        ]
+      },
+      {
+        path: 'products',
+        children: [
+          {
+            path: '',
+            component:ProductsPage
+          }
+        ]
+      },
+      {
+        path: 'aboutshop',
+        children: [
+          {
+            path: '',
+            component:AboutshopPage
+          }
+        ]
+      },
+      {
+        path: 'orders',
+        children: [
+          {
+            path: '',
+            component:OrdersPage
+          }
+        ]
+      },
+      {
+       path: '',
+       redirectTo: '/shop/products',
+        pathMatch: 'full'
+      },
+      
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/shop/products',
+    pathMatch: 'full'
+  }
+];
+
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ShopPageRoutingModule
+    RouterModule.forChild(routes),
+   
   ],
-  declarations: [ShopPage]
+  declarations: [ShopPage,ProductsPage,OrdersPage,GalleryPage,AboutshopPage]
 })
 export class ShopPageModule {}
